76ab09baae77f68b72935bb180ded1a8d0c0c067
Hanier Morales <hsmp16@gmail.com>
2023-07-15 12:02:47 -0600

    change-findOne-method

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -2,10 +2,11 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
-import { Model } from 'mongoose';
+import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
 
@@ -37,8 +38,30 @@ export class PokemonService {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+    let pokemon: Pokemon;
+
+    //si no es un numero
+    if (!isNaN(+term)) {
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+
+    //MongoID
+    if (!pokemon && isValidObjectId(term)) {
+      pokemon = await this.pokemonModel.findById(term);
+    }
+
+    //Para buscar que exista por nombre
+    if (!pokemon) {
+      pokemon = await this.pokemonModel.findOne({ name: term });
+    }
+
+    //Name
+
+    if (!pokemon)
+      throw new NotFoundException(`Pokemon with id, name or no "${term}"`);
+
+    return pokemon;
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
